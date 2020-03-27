86a524bcc94c46575efb298d99e5de8de226d257
João Costa <jjuniorc@gmail.com>
2020-03-27 14:54:08 -0300

    Enemy Chasing

--- Assets/Project/Scripts/Game/ShootingEnemy.cs
+++ Assets/Project/Scripts/Game/ShootingEnemy.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ShootingEnemy : Enemy
 {
     public float shootingInterval = 4f;
     public float shootingDistance = 10f;
     public float shootSpeed = 30;
+    public float chasingInterval = 2f; //Every 2 seconds enemy will check the distance for chase player
+    public float chasingDistance = 12f;
     
     private Player player;
     private float shootingTimer;
+    private float chasingTimer;
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
+        agent = GetComponent<NavMeshAgent>();
         shootingTimer = Random.Range(0, shootingInterval);
+
+        agent.SetDestination(player.transform.position); //Enemy chase the player
     }
 
     // Update is called once per frame
     void Update()
     {
+        //*** SHOOTING LOGIC
         shootingTimer -= Time.deltaTime;
 
         //Only shooting if timer allows and is closer enough to the player
@@ -32,6 +41,16 @@ public class ShootingEnemy : Enemy
             GameObject bullet = ObjectPoolingManager.Instance.GetBullet(false, shootSpeed);
             bullet.transform.position = this.transform.position;
             bullet.transform.forward = (player.transform.position - this.transform.position).normalized;
+        }
+
+        //*** CHASING LOGIC
+        //Chase player check
+        chasingTimer -= Time.deltaTime;
+        if(chasingTimer <= 0)
+        {
+            chasingTimer = chasingInterval;
+
+            agent.SetDestination(player.transform.position); //Enemy chase the player
         }
     }
 }
